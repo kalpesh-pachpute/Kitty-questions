d40012aace92d52c594c358416d347c85fd07c45
kalpesh-pachpute <kalpeshpachpute@gmail.com>
2026-06-13 14:18:44 +0530

    added all files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dev-Kitty
+
+Dev Kitty is a full-stack web application designed to assist developers in honing their skills through a variety of technical and behavioral interview questions. The app also includes a collection of cute dog photos to create a relaxing and enjoyable learning experience. Suitable for developers of all skill levels, Dev Kitty offers a diverse selection of questions for users to practice and improve their abilities.
+
+### Screenshots
+
+<img src="https://github.com/JesacaLin/dev-kitty/blob/main/public/img/dev-kitty-ui.jpg?raw=true" style="width: 950px;">
+
+
+### Tech Stack
+
+JavaScript | Tailwind CSS | Node JS | Express | MongoDB | HTML 5 | CSS 3 | Figma
---
+++ server.js
@@ -0,0 +1,74 @@
+//Declare variables 
+const express = require("express") //returns a function reference, that function is called with express()
+const app = express() //app is an object returned by express();
+const PORT = process.env.PORT || 3000; //setting up the listening port
+const { MongoClient, ObjectId } = require('mongodb');
+// const mongoose = require('mongoose')
+// const DevKittyQ = require('./models/DevKittyQ') //DevKittyQ is where the mongoose models live
+require('dotenv').config()//Things we want to keep private such as connection string to mongodb.
+
+let db,
+dbConnectionStr = process.env.DB_STRING,
+dbName = 'DevKitty';
+
+try {
+  const mongoAtlasLogin = require('./.env/config.js');
+  dbConnectionStr = mongoAtlasLogin.DB_STRING
+} catch(error) {
+  console.error(error)
+}
+
+MongoClient.connect(dbConnectionStr)
+  .then(client => {
+      console.log(`Connected to ${dbName} Database`)
+      db = client.db(dbName)
+
+      app.listen(PORT, () => {
+          console.log(`Server running on port ${PORT}`)
+      })
+  })
+  .catch(err => console.error(err))
+
+//Set middleware
+app.set("view engine", "ejs") //establishing our view engine and ask it to use ejs
+app.use(express.static('public')) //Hey Express, if you need files that are client facing, look in public.
+app.use(express.urlencoded({extended: true})) //helps validate the right type of data back and forth. Extended: true allows us to send more complex objects like arrays etc.
+
+app.get('/', async (req, res) => {
+    const todoItems = await db.collection('DevKittyQuestions').find().toArray()
+    const itemsLeft = await db.collection('DevKittyQuestions').countDocuments()
+    res.render('index.ejs', { items: todoItems, left: itemsLeft })
+});
+
+app.post('/', (req, res) => {
+  db.collection('DevKittyQuestions').insertOne(
+      {
+        category: req.body.category,
+        content: req.body.content
+      })
+  .then(result => {
+      console.log('Question Added')
+      res.redirect('/')
+  })
+  .catch(error => console.error(error))
+})  
+app.get('/remove/:id', async (req, res) => {
+  try {
+    await db.collection('DevKittyQuestions').deleteOne({
+      _id: new ObjectId(req.params.id)
+    });
+
+    console.log('Question Deleted');
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Delete failed');
+  }
+});
+
+
+
+
+
+
+
